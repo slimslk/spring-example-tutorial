d1cd0ef426bd90244bd19c2b659205f39d8c109c
Dimm <dimm@Dimms-MacBook-Pro.local>
2023-10-11 20:52:12 +0200

    Added github deployment workflow and ping controller

---
+++ backend/src/main/java/com/dimm/springbootexample/PingPongController.java
@@ -0,0 +1,15 @@
+package com.dimm.springbootexample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingPongController {
+	private record PingPong(String response){};
+
+	@GetMapping("/ping")
+	public PingPong getPongGesponse(){
+		return new PingPong("pong");
+	}
+
+}
